a36e4953b5f58443a249d52914781db8c9ae7173
Atharv Sharma <atharv5873@gmail.com>
2024-01-15 05:23:20 +0530

    Added a Assignment Question Along with its Solution

---
+++ UOM/Regular Expressions/Assignment/Solution.py
@@ -0,0 +1,8 @@
+import re
+file = open('D:\\Coding\\Python\\Learning-Python\\UOM\\Regular Expressions\\Assignment\\Actual.txt')
+Sum = 0
+for line in file:
+    line = line.rstrip()
+    numbers = re.findall('[0-9]+', line)
+    Sum += sum(map(int, numbers))
+print(Sum)
